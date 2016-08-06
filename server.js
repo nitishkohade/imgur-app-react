8b7c4425a54fb2cc1291d1c1a52f3f662d41a9c7
nitishkohade <nitish.kohade@gmail.com>
2016-08-06 22:28:18 +0530

    [Added express.js as a module in node]

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 
-var server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.end('asdas');
-    
-});
+app.use("/assets", express.static(__dirname));
 
-var port = Number(process.env.PORT || 3000);
+app.get('/', function(req, res){
+    res.send(
+        '<html><head><link rel="stylesheet"'+
+        'href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">'+
+  '<link rel="stylesheet" href="assets/style.css"></head><body>'+  
+  '<div class="container">'+
+  '</div></body><script src="assets/main.js"></script></html>'         
+    );
+})
 
-server.listen(port);
+app.listen(port);
