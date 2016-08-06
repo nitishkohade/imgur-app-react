f99688f1555c807a71348ef90b1141190d182b0e
nitishkohade <nitish.kohade@gmail.com>
2016-08-07 00:29:36 +0530

    [consolidating css files]

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ app.use("/assets", express.static(__dirname));
 
 app.get('/', function(req, res){
     res.send(
-        '<html><head><link rel="stylesheet"'+
-        'href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">'+
-  '<link rel="stylesheet" href="assets/style.css"></head><body>'+  
+        '<html><head>'+
+'<link rel="stylesheet" href="assets/style.css"></head><body>'+  
   '<div class="container">'+
   '</div></body><script src="assets/main.js"></script></html>'         
     );
