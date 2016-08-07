37f74e2a77cdbc8834fc7c4a5be079a694450600
nitishkohade <nitish.kohade@gmail.com>
2016-08-08 02:15:37 +0530

    [updated imgur app]

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use("/assets", express.static(__dirname));
+app.use("", express.static(__dirname));
 
 app.get('/', function(req, res){
     res.send(
         '<html><head>'+
-'<link rel="stylesheet" href="assets/style.css"></head><body>'+  
+'<link rel="shortcut icon" href="img/icon.jpg"/>'+
+'<link rel="stylesheet" href="style.css"></head><body>'+  
   '<div class="container">'+
-  '</div></body><script src="assets/main.js"></script></html>'         
+  '</div></body><script src="main.js"></script></html>'         
     );
 })
 
